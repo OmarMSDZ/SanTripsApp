d6f080e77396430529e6f5545d90316843d9f2d7
Freilin Jose Jerez Brito <freilinjb@gmail.com>
2024-05-13 14:03:08 -0400

    Consultando los registros de la tabla

--- public/assets/js/admin_empleados/admin_empleados.js
+++ public/assets/js/admin_empleados/admin_empleados.js
@@ -1,22 +1,62 @@
 $( function () {
 
+    const PARAMETROS = {
+        URL_DATATABLE: route('empleados.getEmpleados')
+    }
+    var dataTable = $('#tablaEmpleado').DataTable({
+        responsive: true,
+        dom: 'Bfrtip',
+        "ajax" : `${PARAMETROS.URL_DATATABLE}?${$('#formBusqueda').serialize()}`,
+        "language": {
+            // "url": "//cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
+        },
+        "columns": [
+            // {data: 'id'},
+            {data: 'cedula'},
+            {data: 'nombre'},
+            {data: 'apellido'},
+            {data: 'telefono'},
+            {data: 'email'},
+            {data: 'licencia_conducir'},
+            {data: 'fecha_ingreso'},
+            {data: 'fecha_salida'},
+            {data: 'estado'},
+            {data: 'action', name: 'action', orderable: false, searchable: false},
+        ]
+    });
+
+    $('#formBusqueda').submit(function (e) {
+
+        //EVITA QUE SE ACTUALICE LA PAGINA AL ENVIAR LA DATA DE BUSQUEDA
+        e.preventDefault();
+        dataTable.ajax.url(`${PARAMETROS.URL_DATATABLE}?${$('#formBusqueda').serialize()}`).load();
+    });
+
+    //LIMPIA EL FORMULARIO
+    const limpiarFormModal =()=> {
+        $('#codigo_empleado').val('0');
+        $('.limpiarForm').val('');
+        $('.selectLimpiarForm').val('');
+        $('#estado').val('ACTIVO');
+    }
+
     $('#btnNuevoRegistro').click( function () {
+        limpiarFormModal();
+
+
+        $('#btnProcesar').text('GUARDAR');
+        $('#modalRegistroEmpleado  .modal-title').text('Registro de empleado');
 
         $('#modalRegistroEmpleado').modal('show');
     });
+
+
 
     $('#btnProcesar').click( function () {
 
         $("#registroEmpleado").submit();
     });
 
-        //LIMPIA EL FORMULARIO
-        const limpiarFormModal =()=> {
-            $('#codigo_empleado').val('0');
-            $('.limpiarForm').val('');
-            $('.selectLimpiarForm').val('');
-            $('#estado').val('ACTIVO');
-        }
 
     $("#registroEmpleado").validate({
         // rules: {
@@ -87,7 +127,7 @@ $( function () {
 
                 $("#btnProcesar").html("Guardar").prop("disabled", false);
                 //OCULTAR EL MODAL DEL REGISTRO
-                $('#modalRegistroDestino').modal('hide');
+                $('#modalRegistroEmpleado').modal('hide');
 
                 //LIMPIAR EL FORMULARIO
                 limpiarFormModal();
@@ -123,5 +163,40 @@ $( function () {
               },
           });
         },
+    });
+
+    //EVENTO ASOCIADO AL BOTON DE ACTUALIZAR DE LA TABLA
+    $('#tablaEmpleado').on('click', '.btnActualizar', function () {
+
+        $('#btnProcesar').text('ACTUALIZAR');
+        $('#modalRegistroEmpleado  .modal-title').text('Actualizar registro de empleado');
+
+        //CODIGO IDENTIFICADOR DEL REGISTRO
+        const codigo = $(this).attr('codigo');
+
+        //RUTA DE CONSULTA DEL ID DE EMPLEADO
+        const url = route('empleados.getEmpleado', {id_empleado: codigo});
+
+        $.get(url, function (response) {
+
+            const $form = $('#registroEmpleado');
+
+            $form.find('#codigo_empleado').val(response.id);
+            $form.find('input[name="cedula"]').val(response.cedula);
+            $form.find('input[name="nombres"]').val(response.nombre);
+            $form.find('input[name="apellidos"]').val(response.apellido);
+            $form.find('input[name="telefono"]').val(response.telefono);
+            $form.find('input[name="email"]').val(response.email);
+            $form.find('input[name="licencia"]').val(response.licencia_conducir);
+            $form.find('input[name="fechaingreso"]').val(response.fecha_ingreso);
+            $form.find('input[name="fechasalida"]').val(response.fecha_salida);
+            $form.find('#estado').val(response.estado);
+
+            $('#modalRegistroEmpleado').modal('show');
+
+            console.log('response: ', response);
+
+        }, 'json');
+        console.log('codigo: ', codigo);
     });
 });
