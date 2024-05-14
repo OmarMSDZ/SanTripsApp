490feb606a913ce89950682ab839096167dfab55
Freilin Jose Jerez Brito <freilinjb@gmail.com>
2024-05-13 22:49:02 -0400

    Corrigiendo bug de actualizar el registro de empleado y cambiando el campo de la reacion de la tabla de empleados

--- database/migrations/2024_04_25_000023_create_encargadospaquetes_table.php
+++ database/migrations/2024_04_25_000023_create_encargadospaquetes_table.php
@@ -18,18 +18,20 @@ return new class extends Migration
             $table->foreignId('id_paquete_turistico')->constrained('paquetes_turisticos');
 
             // $table->unsignedBigInteger('fk_IdPaquete');
-            $table->unsignedBigInteger('fk_IdEmpleado');
+            $table->foreignId('id_empleado')->constrained('Empleados');
+
+            // $table->unsignedBigInteger('fk_IdEmpleado');
             $table->timestamps();
 
             // $table->index(["fk_IdPaquete"], 'fk_IdPaquete');
 
-            $table->index(["fk_IdEmpleado"], 'fk_IdEmpleado');
+            // $table->index(["fk_IdEmpleado"], 'fk_IdEmpleado');
 
 
-            $table->foreign('fk_IdEmpleado', 'fk_IdEmpleadoEncargado')
-                ->references('IdEmpleado')->on('empleados')
-                ->onDelete('restrict')
-                ->onUpdate('cascade');
+            // $table->foreign('fk_IdEmpleado', 'fk_IdEmpleadoEncargado')
+            //     ->references('IdEmpleado')->on('empleados')
+            //     ->onDelete('restrict')
+            //     ->onUpdate('cascade');
 
             // $table->foreign('fk_IdPaquete', 'fk_IdPaqueteEncargado')
             //     ->references('IdPaquete')->on('paquetes_turisticos')
