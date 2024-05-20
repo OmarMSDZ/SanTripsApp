b8540f07ad075cb92358076c37d34bed88a2b34c
OmarMSDZ <Omarjadismoralesdiaz@gmail.com>
2024-05-19 21:58:23 -0400

    Formulario vehiculos admin

--- database/migrations/2024_04_25_000005_create_modelovehiculo_table.php
+++ database/migrations/2024_04_25_000005_create_modelovehiculo_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             
             $table->id('IdModeloVehiculo');
             $table->string('ModeloVehiculo', 25);
+            $table->foreignId('fk_IdMarcaVehiculo')->nullable()->constrained('marca_vehiculo');;
             $table->timestamps();
         });
     }
