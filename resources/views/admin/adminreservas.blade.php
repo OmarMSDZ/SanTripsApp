f69f2da2fde7d1f56b5a3185c839c0c400459ee2
OmarMSDZ <Omarjadismoralesdiaz@gmail.com>
2024-05-20 22:12:58 -0400

    admin reservas

--- resources/views/admin/adminreservas.blade.php
+++ resources/views/admin/adminreservas.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin_layout_new')
-@section('title', 'Administrador de Ofertas')
+@section('title', 'Administrador de Reservas')
 
 @section('css')
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.0/css/responsive.dataTables.css">
@@ -13,41 +13,100 @@
 
 <div class="page-content p-5" id="content">
 <!-- Contenido de admin reservas -->
-<h2>Admin Reservas <span class="text-gray float-end">SanTrips</span></h2>
-<hr>
-<h2>Reservas Registradas</h2>
-<h3>Mostrar Reservas Por Estado:</h3>
-<select name="clasificacion" id="clasificacion" class="form-select">
-    <option value="PENDIENTE">Pendiente</option>
-    <option value="EN PROCESO">En Proceso</option>
-    <option value="CANCELADA">Cancelada</option>
-    <option value="COMPLETADA">Completada</option>
-</select>
+
+<div class="pagetitle">
+  <h1>Administración de Reservas</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="{{route('adminmenu')}}">Home</a></li>
+      <li class="breadcrumb-item active">Administración de Reservas</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<section class="section">
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Filtro</h5>
+          <div class="row d-flex">
+
+              <div class="col-12">
+
+                <form id="formBusqueda" class="row justify-center align-items-center" method="GET" action="{{ route('reservashechas.index') }}">
+                  <div class="col-md-3 col-sm-4 col-lg-3">
+                      <label for="estado">Estado de la Reservación</label>
+                      <div class="form-group">
+                          <select class="form-select default-select2" name="estado" id="estado" required>
+                              <option value="">Seleccionar Estado</option>
+                              <option value="">Todos</option>
+                              <option value="ACTIVA" {{ request('estado') == 'ACTIVA' ? 'selected' : '' }}>Activa</option>
+                              <option value="EN PROCESO" {{ request('estado') == 'EN PROCESO' ? 'selected' : '' }}>En Proceso</option>
+                              <option value="COMPLETADA" {{ request('estado') == 'COMPLETADA' ? 'selected' : '' }}>Completada</option>
+                              <option value="CANCELADA" {{ request('estado') == 'CANCELADA' ? 'selected' : '' }}>Cancelada</option>
+                          </select>
+                      </div>
+                  </div>
+              
+                  <div class="col-lg-4 mt-3 offset-lg-1">
+                      <div class="">
+                          <button class="btn btn-outline-secondary btn-sm mx-2" type="submit">
+                              <span class="d-sm-inline-block ms-1">
+                                  <i class="bi bi-search"></i> Buscar
+                              </span>
+                          </button>
+                      </div>
+                  </div>
+              </form>
+              
+                  <br>
+                  <hr>
 <!-- esto desde aqui va de forma dinamica, para mostrar una tarjeta con una descripcion breve de la reserva, y un link para proceder a otra pagina donde se detallará mas -->
 <!-- Todo lo que está en asteriscos es informacion dinámica de cada reserva, por cada reserva debe de generarse una tarjeta igual que esta -->
 <!-- El formulario para realizar la reserva está del lado del cliente y no en admin -->
 <!-- Se mostraran por categorias, las cuales se basaran en el estado, ya sea pendientes, en proceso y completado-->
-<div class="row my-5">
-              <div class="col-md-4 stretch-card grid-margin">
-                <div class="card ">
-                  <div class="card-body">
-                    <img src="" class="card-img-absolute" alt="" />
-                    <p hidden>*id*</p>
-                    <h4 class="font-weight-normal mb-3">*breve descripcion del paquete reservado*<i class="mdi mdi-chart-line mdi-24px float-end"></i>
-                    </h4>
-                    <p class="font-weight-normal">Usuario: *nombre del usuario que reserva*</p>
-                    
-                    <p class="font-weight-normal">*Fecha de la reservacion*</p>
-                    
-                    <p class="mb-5">Estatus: *Estatus de la reservacion*</p>
-                    
-                    <!-- Aqui debería enviar con el id del incidente seleccionado a esta pagina -->
-                    <h6 class="card-text"><a href="{{ Route('vistadetalladareserva')}}"  style="text-decoration: none; color: black;">Vista Detallada</a></h6>
-                  </div>
-                </div>
-              </div>
-        </div>
+ 
+<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+  
+  {{-- De esta forma se imprime cada paquete en la pagina luego de hacer la consulta --}}
+  @foreach($reservas as $reserva)
+ 
+      
+  <div class="col">
+      <div class="card h-100">
+          <div class="card-body"> 
+
+              {{-- <p hidden class="id">{{$reserva->idreserva}}</p> --}}
+              <h2 class="card-title">{{$reserva->nombrepaquete}}</h2>
+
+              <p class="card-text"><span style="font-weight:bold">Usuario: </span> {{$reserva->usuario}}</p>
+              <p class="card-text"> <span style="font-weight:bold">Fecha Solicitada: </span> {{$reserva->fechareserva}}</p>
+              <hr>
+              <p class="card-text"> <span style="font-weight:bold">Estado de Reservación: </span> {{$reserva->estado}}</p>
+          
+           
+                   
+                  <form action="{{route('reservashechas.procesarReservaVistaDetallada')}}" method="post">
+                      @csrf <!-- Agrega el token CSRF para protección -->
+                      {{-- En esta parte se pasa el id de la reserva a la otra vista --}}
+                      <input type="text" name="id" value="{{$reserva->idreserva}}" hidden>
+                      <center>
+                      <button type="submit" class="btn mt-3">Ir a Vista Detallada</button>
+                      </center>
+                    </form>  
+                  
+               
+          </div>
+      </div>
+  </div> 
+  @endforeach
+  
+
+<!-- Aquí irían las otras tarjetas de los paquetes turísticos, siguiendo el mismo patrón, se generarían automaticamente desde la BD -->
 </div>
+
+
 
 @endsection
 
--- resources/views/admin/vistadetalladareserva.blade.php
+++ resources/views/admin/vistadetalladareserva.blade.php
@@ -1,34 +1,94 @@
-<x-headadmin></x-headadmin>
+@extends('layouts.admin_layout_new')
+@section('title', 'Administrador de Ofertas')
+
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.0/css/responsive.dataTables.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/colreorder/2.0.0/css/colReorder.dataTables.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.0.1/css/buttons.dataTables.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.0.1/css/buttons.bootstrap5.css">
+
+     {{-- Para permitir eliminar registros --}}
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+@endsection
+
+@section('content')
 <div class="page-content p-5" id="content">
-<!-- Contenido de admin incidentes -->
+<!-- Contenido de admin reserva -->
 <h2>Admin Reservas <span class="text-gray float-end">SanTrips</span></h2>
 <h3>Vista Detallada</h3>
 <hr>
 
-<!-- Todo lo que está en asteriscos es informacion dinámica de cada incidente, por cada incidente debe de generarse una tarjeta igual que esta -->
-<!--La informacion de cada incidente se tomara del id de la tarjeta seleccionada en la vista de los incidentes-->
-<form action="" method="POST" class="form">
+<!-- Todo lo que está en asteriscos es informacion dinámica de cada reserva, por cada reserva debe de generarse una tarjeta igual que esta -->
+<!--La informacion de cada reserva se tomara del id de la tarjeta seleccionada en la vista de las reserva-->
 
-<input name="idincidente" type="text" class="form-control" hidden>
-<h2>Reserva hecha por: *Usuario*</h2> 
-<p>Para el: *Fecha*</p>
-<p>Estado: *Estado*</p>
-<p>Cantidad de personas: *cantidad de personas*</p>
+@foreach ($reservas as $reserva)
+ <form action="{{route('reservashechas.update', $reserva->idreserva)}}" method="POST" class="form" id="vistadetallada">    
+
+@csrf
+<h2>Reserva Hecha Por: {{$reserva->usuario}}</h2> 
+
+<p id="codigo" hidden>{{$reserva->idreserva}}</p>
+<p><span style="font-weight:bold">Paquete Reservado: </span>{{$reserva->nombrepaquete}}</p>
+<p><span style="font-weight:bold">Para la Fecha:</span> {{$reserva->fechareserva}}</p>
+<p><span style="font-weight:bold">Cantidad de Personas:</span> {{$reserva->cantpersonasreserva}}</p>
+<p><span style="font-weight:bold">Total (RD$):</span> {{$reserva->montoreserva}}</p>
+<p><span style="font-weight:bold">Estado de Reserva: </span>{{$reserva->estado}}</p>
+
+
 <hr>
-<label for="estado"> <h4> Actualizar estado de la reserva</h4></label>
+
+<label for="estado"> <h4> Actualizar estado de la Reservación</h4></label>
 <select name="estado" id="" class="form-select">
-    <option value="PENDIENTE">Pendiente</option>
+    <option value="ACTIVA">Activa</option>
     <option value="EN PROCESO">En Proceso</option>
     <option value="CANCELADA">Cancelada</option>
     <option value="COMPLETADA">Completada</option>
 </select>
 
 <br>
-<button type="submit" class="btn btn-success">Actualizar Estado Reserva</button>
-<br>
 
-<p class="font-weight-bold text-uppercase px-3 small mt-3 mb-0">Otras Acciones</p>
-<button type="button" class="btn btn-primary mt-1" style="width: 14em;"> <a href="{{ Route('adminreservas')}}"  style="text-decoration: none; color: white;">Ir a Admin Reservas</a></button>
+ 
+<button type="submit" class="btn btn-success">Actualizar Estado Reserva</button>
+ 
 </form>
+
+<!-- Formulario de eliminación -->
+<form action="{{ route('reservashechas.destroy', $reserva->idreserva) }}" method="POST" class="form-delete">
+  @csrf
+  @method('DELETE')
+  <br>
+  <button type="submit" class="btn btn-danger btnEliminar">
+      <i class="bi bi-trash"></i> Eliminar
+  </button>
+</form>
+
+ 
+<br>
+ 
+@endforeach
+
 </div>
-<x-footeradmin></x-footeradmin>
+
+
+@endsection
+
+@section('javascript')
+  <script src="https://cdn.datatables.net/2.0.1/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.0.1/js/dataTables.bootstrap5.js"></script>
+  <script src="https://cdn.datatables.net/colreorder/2.0.0/js/colReorder.dataTables.js"></script>
+  <script src="https://cdn.datatables.net/responsive/3.0.0/js/dataTables.responsive.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.0.1/js/dataTables.buttons.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.dataTables.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.print.min.js"></script>
+  <script src="https://cdn.datatables.net/2.0.2/js/dataTables.bootstrap5.js"></script>
+  <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.bootstrap5.js"></script>
+
+  {{-- IMPORTANTE, INCLUIR EL JS CORRESPONDIENTE A ESTA VISTA PARA QUE FUNCIONE DE MANERA ADECUADA --}}
+   <script src="{{asset('assets/js/admin_reservas/admin_reservas.js')}}"></script>  
+
+@endsection
